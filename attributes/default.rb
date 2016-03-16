6bb062763e137435b7906b27caa47b35bf6a9147
Jeff Yaskus <jeffyaskus@gmail.com>
2016-03-15 20:25:24 -0700

    removed nord specific data

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,7 +12,7 @@ default['netbackup']['rhel'] = {
 
   client_base_ver: '7.5',
   client_base_dir: 'NetBackup_7.5_CLIENTS',
-  client_base_url: 'https://mvnrepo.nordstrom.net/nexus/content/repositories/thirdparty/symantec/com/netbackup_client_redhat/7.5.0.7',
+  client_base_url: 'http://hostingsite.com/netbackup_client_redhat/7.5.0.7',
   client_base_tar: 'netbackup_client_redhat-7.5.0.7-base.tar',
   client_base_tar_kb: '700000',
   client_base_sum: 'e5723ccc9d4aefffbd52f24f39024bbed2ec4ed99292505cc6d1b1e0308c9253',
@@ -22,7 +22,7 @@ default['netbackup']['rhel'] = {
   client_patch_dir: 'NB_update',
   client_patch_tar: 'netbackup_client_redhat-7.5.0.7-patch.tar',
   client_patch_tar_kb: '400000',
-  client_patch_url: 'https://mvnrepo.nordstrom.net/nexus/content/repositories/thirdparty/symantec/com/netbackup_client_redhat/7.5.0.7',
+  client_patch_url: 'http://hostingsite.com/netbackup_client_redhat/7.5.0.7',
   client_patch_sum: '5e7dd2377618d10bab3d5cb0a193c821109109b2f432921b90823bd8a9506979',
   client_patch_pkg: 'NB_CLT_7.5.0.7',
   client_patch_response: 'response.patch',
@@ -31,7 +31,7 @@ default['netbackup']['rhel'] = {
   client_appliance_dir: 'SYMCnbclient_Linux-RedHat2.6.18_7.6.1.1',
   client_appliance_tar: 'netbackup_client_redhat-7.6.1.1.tar',
   client_appliance_tar_kb: '500000',
-  client_appliance_url: 'https://mvnrepo.nordstrom.net/nexus/content/repositories/thirdparty/symantec/com/netbackup_client_redhat/7.6.1.1',
+  client_appliance_url: 'http://hostingsite.com/netbackup_client_redhat/7.6.1.1',
   client_appliance_sum: 'fc894a7072fec98b5a2c0a51d7d098b015709288eff59e6fa3debb1e42c2e879',
   client_appliance_pkg: 'NB_CLT_7.6.1.1',
   client_appliance_response: 'response.install'
@@ -44,7 +44,7 @@ default['netbackup']['solaris2'] = {
 
   client_base_ver: '7.5',
   client_base_dir: 'NetBackup_7.5_CLIENTS',
-  client_base_url: 'https://mvnrepo.nordstrom.net/nexus/content/repositories/thirdparty/symantec/com/netbackup_client_solaris/7.5',
+  client_base_url: 'http://hostingsite.com/netbackup_client_solaris/7.5',
   client_base_tar: 'netbackup_client_solaris-7.5.tar',
   client_base_tar_kb: '600000',
   client_base_sum: '4c38bb8811e1952a18838bcb378b70beb6019dfc66d4294f2ff39ee48c7130d4',
@@ -54,7 +54,7 @@ default['netbackup']['solaris2'] = {
   client_patch_dir: 'NB_CLT_7.5.0.7',
   client_patch_tar: 'netbackup_client_solaris-7.5.0.7.tar',
   client_patch_tar_kb: '1000000',
-  client_patch_url: 'https://mvnrepo.nordstrom.net/nexus/content/repositories/thirdparty/symantec/com/netbackup_client_solaris/7.5.0.7',
+  client_patch_url: 'http://hostingsite.com/netbackup_client_solaris/7.5.0.7',
   client_patch_sum: 'c9657206ff36dacfb396f60e25ece1fc227892606aa6124259780dda2407602c',
   client_patch_pkg: 'NB_CLT_7.5.0.7',
   client_patch_response: 'response.patch',
@@ -63,7 +63,7 @@ default['netbackup']['solaris2'] = {
   client_appliance_dir: 'netbackup_client_solaris_10_x86_v7611',
   client_appliance_tar: 'netbackup_client_solaris-7.6.1.1.tar',
   client_appliance_tar_kb: '900000',
-  client_appliance_url: 'https://mvnrepo.nordstrom.net/nexus/content/repositories/thirdparty/symantec/com/netbackup_client_solaris/7.6.1.1',
+  client_appliance_url: 'http://hostingsite.com/netbackup_client_solaris/7.6.1.1',
   client_appliance_sum: 'edeead17bc0d57103e4d05186feb78119e3b2ee9933990dd4df13f5ca8477556',
   client_appliance_pkg: 'NB_CLT_7.6.1.1',
   client_appliance_response: 'response.install'
--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -27,68 +27,12 @@ module NetBackup
       # associated DNS entry for the tagged vlan
       # that are configured on the netbackup servers
       tagged_vlans = {
-        # p1
-        '10.24.0.64/26'    => 'sa0319nbu01-2409',
-        '10.24.8.0/23'     => 'sa0319nbu01-2411',
-        '10.24.192.0/26'   => 'sa0319nbu01-2551',
-        '10.24.192.64/26'  => 'sa0319nbu01-2552',
-        '10.24.192.128/26' => 'sa0319nbu01-2553',
-        '10.24.192.192/26' => 'sa0319nbu01-2554',
-        '10.24.193.0/26'   => 'sa0319nbu01-2555',
-        '10.24.193.64/26'  => 'sa0319nbu01-2556',
-        '10.24.193.128/26' => 'sa0319nbu01-2557',
-        '10.24.193.192/27' => 'sa0319nbu01-2558',
-        '10.24.193.224/27' => 'sa0319nbu01-2559',
-        '10.24.194.0/27'   => 'sa0319nbu01-2560',
-        '10.24.194.32/27'  => 'sa0319nbu01-2561',
-        '10.24.194.64/27'  => 'sa0319nbu01-2562',
-        '10.24.194.96/27'  => 'sa0319nbu01-2563',
-        '10.24.194.128/27' => 'sa0319nbu01-2564',
-        '10.24.196.0/23'   => 'sa0319nbu01-2565',
-        '10.24.198.0/23'   => 'sa0319nbu01-2566',
-        '10.24.194.176/28' => 'sa0319nbu01-2568',
-        # 319
-        '10.16.34.0/23'    => 'sa0319nbu03-0522',
-        '10.16.64.0/23'    => 'sa0319nbu03-0620',
-        '10.16.66.0/23'    => 'sa0319nbu03-0622',
-        '10.16.57.0/25'    => 'sa0319nbu03-0642',
-        '10.16.58.0/27'    => 'sa0319nbu03-0644',
-        # yp37 (non-PCI)
-        '10.16.32.0/23'    => 'y0319p37-v520',
-        '10.16.37.32/28'   => 'y0319p37-v528',
-        '10.16.37.48/28'   => 'y0319p37-v529',
-        '10.16.45.48/28'   => 'y0319p37-v585',
-        '10.16.58.64/28'   => 'y0319p37-v646',
-        '10.16.58.80/28'   => 'y0319p37-v647',
-        '10.16.69.0/27'    => 'y0319p37-v626',
-        '10.16.69.32/28'   => 'y0319p37-v627',
-        '10.16.69.64/27'   => 'y0319p37-v628',
-        '10.16.78.0/23'    => 'y0319p37-v685',
-        '10.16.246.128/25' => 'y0319p37-v797',
-        # yp86 (PCI)
-        '10.1.89.0/24'      => 'y0319p86-v925',
-        '10.16.40.0/23'     => 'y0319p86-v560',
-        '10.16.42.0/23'     => 'y0319p86-v562',
-        '10.16.45.0/28'     => 'y0319p86-v566',
-        '10.16.45.96/28'    => 'y0319p86-v571',
-        '10.16.45.112/28'   => 'y0319p86-v572',
-        '10.16.45.128/27'   => 'y0319p86-v573',
-        '10.16.45.160/28'   => 'y0319p86-v574',
-        '10.16.45.192/27'   => 'y0319p86-v575',
-        '10.16.45.176/28'   => 'y0319p86-v576',
-        '10.16.72.0/23'     => 'y0319p86-v660',
-        '10.16.74.0/23'     => 'y0319p86-v662',
-        '10.16.76.0/25'     => 'y0319p86-v664',
-        '10.16.77.0/26'     => 'y0319p86-v666',
-        '10.16.77.128/28'   => 'y0319p86-v669',
-        '10.16.77.144/28'   => 'y0319p86-v670',
-        '10.16.78.0/23'     => 'y0319p86-v685',
-        '10.16.212.0/25'    => 'y0319p86-v942',
-        '10.16.212.128/25'  => 'y0319p86-v943',
-        '10.16.213.128/25'  => 'y0319p86-v945',
-        '10.16.247.128/25'  => 'y0319p86-v799',
-        '161.181.52.128/27' => 'y0319p86-v830',
-        '161.181.243.0/25'  => 'y0319p86-v795'
+        # environment 1
+        '10.16.32.0/23'    => 'media1_1-v032',
+        '10.16.64.0/25'    => 'media1_1-v064',
+        # environment 2
+        '10.16.32.0/25'  => 'media2_1-v032',
+        '10.16.64.0/25'  => 'media2_1-v064'
       }
       # make sure it returns nil if not found
       tagged_vlans.default = nil
@@ -364,43 +308,32 @@ module NetBackup
 
     def netbackup_env_servers(master_server)
       case master_server
-      when 'x0319p09'
-        servers_legacy_nonpci
-      when 'y0319p85'
-        servers_legacy_pci
-      when 'sa0319nbu01'
-        servers_p1
-      when 'sa0319nbu02'
-        servers_319
-      when 'sa0864nbu01'
-        servers_864
-      when 'sa0606nbu01'
-        servers_606
+      when 'master1'
+        servers_environment_a
+      when 'master2'
+        servers_environment_b
+      when 'master3'
+        servers_environment_c
+      when 'master4'
+        servers_environment_d
       end
     end
 
-    def servers_legacy_pci
-      %w(y0319p85 y0319p86 a0319p279)
+    def servers_environment_a
+      %w(master1 media1_1 media1_2)
     end
 
-    def servers_319
-      %w(sa0319nbu02 sa0319nbu03 sa0319nbu02.nordstrom.net sa0319nbu03.nordstrom.net)
+    def servers_environment_b
+      %w(master2 media2_1 media2_2)
     end
 
-    def servers_864
-      %w(sa0864nbu01 sa0864nbu01.nordstrom.net)
+    def servers_environment_c
+      %w(master3 media3_1 media3_2)
     end
 
-    def servers_p1
-      %w(sa0319nbu01 sa0319nbu01.nordstrom.net)
+    def servers_environment_d
+      %w(master4 media4_1 media4_2)
     end
 
-    def servers_legacy_nonpci
-      %w(x0319p09 y0319p37 y0319p286 a0319p281)
-    end
-
-    def servers_606
-      %w(sa0606nbu01 sa0606nbu01.nordstrom.net)
-    end
   end
 end
